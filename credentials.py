5efb9576e580e429a308d7f53a0bb7c4cfb0d5b0
Mosesvalei <moses.mwanzia@student.moringaschool.com>
2021-08-06 17:18:19 +0300

    classmethod decorator

--- credentials.py
+++ credentials.py
@@ -22,4 +22,23 @@ class Credential:
         Credential.credential_list.append(self)
 
 
-   
+    @classmethod
+    def authenticate_credential (cls,name,password):
+        '''
+        Method that checks if user and password are correct
+        '''
+        for cred in cls.credential_list:
+            if cred.user_name == name and cred.password == password:
+                return cred
+        return 0
+
+    def cred_data_exists(cls,number):
+        '''
+        Checks if data exists in the profile
+        '''
+        for data in cls.users_list:
+            if data.data_identity == number:
+                return True
+        return False
+
+    #End of credential class
