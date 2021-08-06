b6f33821b0e8c02c581b734af051f77beb971deb
Mosesvalei <moses.mwanzia@student.moringaschool.com>
2021-08-06 17:43:11 +0300

    test

--- userdata_test.py
+++ userdata_test.py
@@ -34,4 +34,41 @@ class TestUserData(unittest.TestCase):
         self.new_userdata.save_account()   #create and save new_cred
         self.assertEqual(len(UserData.users_list),1)
 
+    def test_password_generator(self):
+        '''
+        test_password_generator test case to test if password has been generated and saved
+        '''
+        password_list = []
+
+        password = self.new_userdata.password_generator(2)
+        password_list.append(password)
+        self.assertEqual(len(password_list),1)
+
+    def test_data_exists(self):
+        '''
+        test_data_exists test case to test if the the function checks that the data exists
+        '''
+
+        self.new_userdata.save_account()
+        test_data = UserData(2,2,"Test","admintest")
+
+        test_data.save_account()
+        data_exists = UserData.data_exists(2)
+        self.assertTrue(data_exists)
+
+    def test_display_data(self):
+        '''
+        test_display_data test case used to test if the function displays the data
+        '''
+        self.new_userdata.save_account()
+        test_data = UserData(2,2,"Test","admintest")
+
+        test_data.save_account()
+        display_data = UserData.display_data(2,2)
+        self.assertEqual(test_data.account_name,display_data.account_name)
+
+    def test_copy_password(self):
+        '''
+        Test to confirm that we are copying the email address from a found contact
+        '''
 
