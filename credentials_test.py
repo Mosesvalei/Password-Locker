c26bf63adad3e4f6bce11e0d6a265e18d20fcc65
Mosesvalei <moses.mwanzia@student.moringaschool.com>
2021-08-06 17:20:51 +0300

    test credentials

--- credentials_test.py
+++ credentials_test.py
@@ -26,4 +26,33 @@ class TestCredential(unittest.TestCase):
         self.assertEqual(self.new_cred.user_name,"Sophia")
         self.assertEqual(self.new_cred.password,"admin")
 
+    def test_save_credential(self):
+        '''
+        test_create_credential test case to test if credential object is saved into credential_list
+        '''
+        self.new_cred.save_credential()   #create and save new_cred
+        self.assertEqual(len(Credential.credential_list),1)
 
+    def test_create_multiple_credentials(self):
+        '''
+        test_create_multiple_credentials test case to check if we can save multiple objects to our credential list
+        '''
+        self.new_cred.save_credential()
+        test_cred = Credential(2,"Njeri","admin2") #new credentials
+        test_cred.save_credential()
+
+        self.assertEqual(len(Credential.credential_list),2)
+
+    def test_authenticate_credential(self):
+        '''
+        '''
+
+        test_cred = Credential(1,"Test","Password")
+        test_cred.save_credential()
+
+        found_cred = Credential.authenticate_credential("Test","Password")
+        self.assertEqual(found_cred.identity,test_cred.identity)
+
+
+if __name__ == "__main__":
+    unittest.main()
